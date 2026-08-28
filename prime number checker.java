9d2178c220aec0ed80890c32d2d144792e19d21b
kr5818-sudo <kr5818@srmist.edu.in>
2026-08-28 21:47:30 +0530

    Add prime number checker implementation

---
+++ prime number checker.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PrimeChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        boolean isPrime = true;
+
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Not Prime");
+        }
+
+        sc.close();
+    }
+}
